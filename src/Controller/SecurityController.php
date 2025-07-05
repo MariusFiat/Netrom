8e691b4336115f69fcfa4254e90ad23a217e7eb6
Ion Fiat <ion.fiat@student.upt.ro>
2025-07-06 02:18:32 +0300

    Register section

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Entity\UserDetails;
+use App\Form\RegisterFormType;
+use App\Form\RegistrationForm;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -12,10 +20,11 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
+//        if ($this->getUser()) {
+//            return $this->redirectToRoute('homepage');
+//        }
 
-        // last username entered by the user
+        $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('login.html.twig', [
@@ -27,6 +36,63 @@ class SecurityController extends AbstractController
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        throw new \LogicException('This method will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $entityManager,
+        Security $security
+    ): Response {
+        // Redirect if already logged in
+//        if ($this->getUser()) {
+//            return $this->redirectToRoute('app_home');
+//        }
+
+        $user = new User();
+        $form = $this->createForm(RegistrationForm::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                // Create and set UserDetails
+                $userDetails = new UserDetails();
+                $userDetails
+                    ->setFirstName('')
+                    ->setLastName('')
+                    ->setAge(0)
+                    ->setRegisterDate(new \DateTime())
+                    ->setLastConnection(new \DateTime());
+
+                $user
+                    ->setPassword($passwordHasher->hashPassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    ))
+                    ->setToken(bin2hex(random_bytes(18)))
+                    ->setRoles(['ROLE_USER'])
+                    ->setUserDetails($userDetails);
+
+                // Persist both entities
+                $entityManager->persist($userDetails);
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                return $security->login($user, 'form_login', 'main');
+
+            } catch (\Exception $e) {
+                $this->addFlash('error', 'Registration failed: '.$e->getMessage());
+            }
+        } elseif ($form->isSubmitted() && !$form->isValid()) {
+            foreach ($form->getErrors(true) as $error) {
+                $this->addFlash('error', $error->getMessage());
+            }
+        }
+
+        return $this->render('register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,10 +6,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements PasswordAuthenticatedUserInterface, UserInterface
 {
     #[ORM\Id]
@@ -87,16 +89,13 @@ class User implements PasswordAuthenticatedUserInterface, UserInterface
 
     public function getRoles(): array
     {
-        //$roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
-
         return array_unique($roles);
     }
 
-    public function setRole(string $role): static
+    public function setRoles(array $role): static
     {
-        $this->role = $role;
+        $this->roles = $role;
 
         return $this;
     }
